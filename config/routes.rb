8d8342bc5713a1639e81cbf5540f18a83f36e7b5
clement duchemin <clement_mz@protonmail.com>
2018-05-11 12:53:39 +0200

    Prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-
+root to: 'cocktails#index'
+  resources :cocktails do
+    resources :doses, only: [ :index, :new, :create ], shallow: true
+  end
+# resources :doses, only: [ :show, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
